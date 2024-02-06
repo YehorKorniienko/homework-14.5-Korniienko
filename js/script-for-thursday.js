4db7865c3429b5851c313c8e320c7bc17999cff1
Yehor <augortv@gmail.com>
2024-02-06 19:57:15 +0200

    first commit

---
+++ js/script-for-thursday.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
